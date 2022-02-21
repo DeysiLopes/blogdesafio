1fb86084bb628bdf9d8e85364d8565190cc93d3d
DeysiLopes <deysilinconl@gmail.com>
2022-02-20 23:18:16 -0300

    Create database connection in the application

---
+++ src/main/java/com/deysi/blogdesafio/BlogdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.deysi.blogdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogdesafioApplication.class, args);
+	}
+
+}
